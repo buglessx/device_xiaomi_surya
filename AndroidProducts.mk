9d67c5a8187a63336dbf7c69c4dba4a819ee8241
buglessx <muhammad.065117057@unpak.ac.id>
2022-04-22 07:01:38 +0000

    surya: nad-ify
    Signed-off-by: buglessx <muhammad.065117057@unpak.ac.id>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_surya.mk
+    $(LOCAL_DIR)/nad_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    carbon_surya-user \
-    carbon_surya-userdebug \
-    carbon_surya-eng
+    nad_surya-user \
+    nad_surya-userdebug \
+    nad_surya-eng
